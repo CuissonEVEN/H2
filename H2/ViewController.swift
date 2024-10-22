c9e62cf46fea14847c2a357d32affe870f260ed6
chaehyunp <chhue96@gmail.com>
2024-10-22 15:47:17 +0900

    first commit

---
+++ H2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  H2
+//
+//  Created by CHYUN on 10/21/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ H2Tests/H2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  H2Tests.swift
+//  H2Tests
+//
+//  Created by CHYUN on 10/21/24.
+//
+
+import Testing
+@testable import H2
+
+struct H2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
